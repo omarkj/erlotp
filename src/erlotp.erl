c1483a79c1a76027d131eeb86e19d68b1fa36273
andrey <andrey.v.yanovsky@gmail.com>
2014-07-08 12:56:18 +0200

    tiny change: hmac/3 instead outdated sha_mac/2

--- src/erlotp.erl
+++ src/erlotp.erl
@@ -6,11 +6,24 @@
 -type interval() :: integer().
 
 -export([get_hotp/2,
-	 get_totp/1]).
+	 get_totp/1,
+	 secret/1]).
 
 -export_type([token/0,
 	      secret/0,
 	      interval/0]).
+
+
+%% @doc
+%% Random secret, N bytes
+%% @end
+-spec secret(integer()) -> secret().
+secret(N) when N < 16 ->
+    throw({error, unsafe_secret});
+secret(N) ->
+    Key = crypto:rand_bytes(N),
+    Secret = erlotp_base32:encode(Key),
+    Secret.
 
 %% @doc
 %% Get a one time token based on a secret and a 
@@ -20,7 +33,7 @@
 get_hotp(Secret, Interval) when is_binary(Secret),
 				is_integer(Interval) ->
     Key = erlotp_base32:decode(Secret),
-    Digest = crypto:sha_mac(Key, <<Interval:64/integer>>),
+    Digest = crypto:hmac(sha, Key, <<Interval:64/integer>>),
     <<_:19/bytes,_:4/bits,Offset:4>> = Digest,
     <<_:Offset/bytes,_:1/bits,P:31,_/binary>> = Digest,
     P rem 1000000.
